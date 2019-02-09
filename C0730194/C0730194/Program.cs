5d8eff3735bcd76317c97ef1e4294d16b00ebc72
visualstudio <visualstudio@122-PC39>
2019-02-08 20:40:31 -0500

    8:30 Commit

--- C0730194/C0730194/Program.cs
+++ C0730194/C0730194/Program.cs
@@ -11,32 +11,39 @@ namespace C0730194
         static void Main(string[] args)
         {
             Elevator e = new Elevator();
-            e.run();
+            e.setup();
+            e.TraverseList();
         }
     }
 
     class Node
     {
         public Node() { }
-        public Node floor;
         public Node elevatorUp;
         public string FloorNumber;
     }
 
+    class Department
+    {
+        public Department aDepartment;
+
+    }
+
     class Elevator
     {
+        Node Head;
         Node FirstFloor;
         Node SecondFloor;
         Node ThirdFloor;
         Node FourthFloor;
 
-        public void run()
+        public void setup()
         {
             FirstFloor = new Node();
             SecondFloor = new Node();
             ThirdFloor = new Node();
             FourthFloor = new Node();
-
+            Head = FirstFloor;
             FirstFloor.FloorNumber = "First Floor";
             FirstFloor.elevatorUp = SecondFloor;
             SecondFloor.FloorNumber = "Second Floor";
@@ -45,6 +52,22 @@ namespace C0730194
             ThirdFloor.elevatorUp = FourthFloor;
             FourthFloor.FloorNumber = "Fourth Floor";
             FourthFloor.elevatorUp = null;
+        }
+
+        public void TraverseList()
+        {
+            Node temp;
+            temp = Head;
+
+            // where am I going to start?
+
+            while (temp != null)
+            {
+                Console.WriteLine(temp.FloorNumber);
+                temp = temp.elevatorUp;
+
+            }
+
         }
     }
 }
